ca49add25565ffdf7fa50c78169b83027ca3388a
iaejotape <gamesjoaopedro43@gmail.com>
2025-12-02 10:16:39 -0300

    att

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'app-overflow',
+  appName: 'Overflow',
   webDir: 'www',
   android: {
     // Garantir que o conteúdo não fique atrás das barras do sistema
